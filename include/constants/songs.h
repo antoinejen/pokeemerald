ece89b24ec85e62297d1ccad88ff7fe693b42ff8
Rixlanchy <rixlanchy@gmail.com>
2025-04-15 21:58:18 +0200

    Revert Team Rocket music

--- include/constants/songs.h
+++ include/constants/songs.h
@@ -546,8 +546,6 @@
 #define PH_NURSE_HELD               608
 #define PH_NURSE_SOLO               609
 
-#define MUS_TEAM_ROCKET             610
-
 #define MUS_ROUTE118                0x7FFF  // Map is split into 2 music sections. controlled by GetCurrLocationDefaultMusic().
 
 #define MUS_NONE                    0xFFFF
